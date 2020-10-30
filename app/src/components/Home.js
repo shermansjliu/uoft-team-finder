99e25ab877d9428f81d3135a3aa965f2fdac1229
Sherman <shermanliu03@gmail.com>
2020-10-30 16:08:09 -0400

    added state to HomeView

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,50 +1,91 @@
-import React, {Component} from "react";
-import {Layout, Space, Typography} from 'antd'
+import React, { Component } from "react";
+import { Layout, Space, Typography, Menu } from "antd";
+import { Link } from "react-router-dom";
+import TeamCard from "./TeamCard";
+import { SettingOutlined, TeamOutlined } from "@ant-design/icons";
+import { User, Group } from "grommet-icons";
+import "../App.css";
 
-import {SettingOutlined, TeamOutlined} from '@ant-design/icons'
-
-import '../App.css'
-
-const {Sider, Content, Footer} = Layout
-const {Title} = Typography
+const { Sider, Content, Footer } = Layout;
+const { Title } = Typography;
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      teams: [
+        {
+          teamName: "The John Wicks",
+          members: [
+            { userID: "DavidID", name: "David" },
+            { userID: "ShermanID", name: "Sherman" },
+            { userID: "QuincyID", name: "Quincy" },
+            { userID: "JesseID", name: "Jesse" },
+          ],
+          capacity: 4,
+        },
+        {
+          teamName: "BA Forever",
+          members: [],
+          capacity: 4,
+        },
+      ],
+    };
+  }
   render() {
-    return  (
-     <div> 
-      <Layout className="homeViewContainer">
+    return (
+      <div>
+        <Layout className="homeViewContainer">
+          <Sider>
+            <div className="homeViewSideMenu">
+              <div>
+                <Title className="classTitle" level={2}>
+                  CSC236
+                </Title>
+                <Title className="totalTeams" level={3}>
+                  {" "}
+                  Teams: 10
+                </Title>
 
-        <Sider >
-          <div className="homeViewSideMenu">
-            <div>
-        <Title className="classTitle" level={2}>CSC236</Title>
-        <Title className="totalTeams" level={3}> Teams: 10</Title>
-
-        <Space direction="vertical">
-        <Title level={5}> Project: 5</Title>
-        <Title level={5}> Study: 5</Title>
-      
-        </Space>
-        </div>
-        {/* <Layout> */}
-          <footer className="homeViewFooter">
-            <ul>
-              
-           <li className="footerItem"><SettingOutlined /></li>
-           <li className="footerItem"><TeamOutlined /></li>
-           <li className="footerItem"></li>
-            </ul>
-        
-         </footer>
-         {/* </Layout> */}
-       
-         </div>
-        </Sider>
-        <Content hasSider={true} className="homeViewContent">Content</Content>
-      </Layout>
+                <Space direction="vertical">
+                  <Title level={5}> Project: 5</Title>
+                  <Title level={5}> Study: 5</Title>
+                </Space>
+              </div>
+              <footer className="homeViewFooter">
+                <ul>
+                  <Link to="/Setting">
+                    <li className="footerItem">
+                      <SettingOutlined />
+                    </li>
+                  </Link>
+                  <Link to="/Team">
+                    <li className="footerItem">
+                      <Group />
+                    </li>
+                  </Link>
+                  <Link>
+                    <li className="footerItem">
+                      <User />
+                    </li>
+                  </Link>
+                </ul>
+              </footer>
+            </div>
+          </Sider>
+          <Content hasSider={true} className="homeViewContent">
+            {this.state.teams.map((team) => {
+              return (
+                <TeamCard
+                  teamName={team.teamName}
+                  members={team.members}
+                  capacity={team.capacity}
+                />
+              );
+            })}
+          </Content>
+        </Layout>
       </div>
-    )
-
-
+    );
   }
 }
 
