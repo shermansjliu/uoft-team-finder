35f0ec855c6f4a2a8710426645a6ac0e842d82a2
qinxi yu <quincyouxiang@gmail.com>
2020-11-02 15:11:10 -0500

    add profile image

---
+++ app/src/components/Profile/Profile_image.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Avatar} from 'antd';
+import 'antd/dist/antd.css';
+import { UserOutlined } from '@ant-design/icons';
+import style from './style.css';
+
+class Profile_image extends React.Component {
+    render() {
+        return (
+            <div id='profile_curve' style={{
+                borderRadius: "50%",
+                position: "relative",
+                width: "200px",
+                height: "200px"
+            }}>
+                <Avatar icon={<UserOutlined />} size={200}
+                    style={{
+                        color: '#FFFAF0',
+                        backgroundColor: "#87CEFA"}}> 
+                </Avatar>
+            </div>
+        )
+    }
+}
+
+export default Profile_image;
