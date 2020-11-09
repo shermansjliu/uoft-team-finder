ade960564db6bf61ae9f1cf27aa7d7e4c6cc46e2
Jun Da Wang <junda.wang@mail.utoronto.ca>
2020-11-09 14:50:04 -0500

    display description of team members

--- app/src/components/Team/admin/TeamLeader.js
+++ app/src/components/Team/admin/TeamLeader.js
@@ -41,6 +41,7 @@ class TeamLeader extends React.Component {
             />
           </div>
           <div className="memberButtonsContainer">{renderAdminButtons()}</div>
+          <div className="teamLeaderDescription">{teamLeader.description}</div>
         </Card>
       </div>
     );
