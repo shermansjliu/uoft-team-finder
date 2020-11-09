cf0482e32fde0b23d09e9d91260d8e5696384a67
Jessexu <yexiong.xu@mail.utoronto.ca>
2020-11-09 16:15:45 -0500

    Change Home Icon

--- app/src/components/AdminUserTable/AdminUserTable.js
+++ app/src/components/AdminUserTable/AdminUserTable.js
@@ -4,26 +4,7 @@ import {Input, Space, Table, Tag} from "antd";
 import {SearchOutlined} from "@ant-design/icons";
 import "./style.css"
 import {Link} from "react-router-dom";
-
-
-
-function changePassword(record) {
-    let newPassword = prompt('Please Enter new password for this user')
-    if (newPassword){
-        record.password = newPassword
-    }
-    console.log("password changed",record)
-}
-
-function deleteUser(record, page) {
-    const filteredDate = page.state.data.filter((item) => {
-        return item.username !== record.username
-    })
-    page.setState({data:filteredDate})
-    console.log("deleteUser",record.username)
-}
-
-
+import {changePassword, deleteUser} from "./actions"
 
 class AdminUserTable extends React.Component{
     state = {
