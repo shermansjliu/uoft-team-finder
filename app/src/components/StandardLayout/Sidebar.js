a041a128b03d8b5168491b2772c2015944836705
Sherman <shermanliu03@gmail.com>
2020-11-09 17:06:30 -0500

    added create team form

--- app/src/components/StandardLayout/Sidebar.js
+++ app/src/components/StandardLayout/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Layout, Image, Tooltip } from "antd";
+import { Layout, Modal, Image, Tooltip, Button } from "antd";
 import { Link } from "react-router-dom";
 import {
   LogoutOutlined,
@@ -8,45 +8,82 @@ import {
   PlusOutlined,
 } from "@ant-design/icons";
 import "./index.css";
+import CreateTeamForm from "./CreateTeamForm";
 import profilePic from "./profile_pic.jpg";
 export default class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modalVisible: false,
+    };
+  }
+  openModal = () => {
+    this.setState({ modalVisible: true });
+  };
+  handleOk = () => {
+    this.setState({ modalVisible: false });
+  };
+  handleCancel = () => {
+    this.setState({ modalVisible: false });
+  };
   render() {
     const { image } = this.props;
     const { Sider } = Layout;
-    return (
-      <Sider
-        className="sideBar theme-background-color"
-        width={120}
-        collapsible={true}
-        collapsedWidth={0}
-      >
-        <div className="sb-profilePictureContainer">
-          <Image src={profilePic} alt="profile picture" />
-        </div>
-        <div className="sideBarButtons">
-          <Link>
-            <Tooltip placement="right" title="New Team">
-              <PlusOutlined className="sb-btn" />
-            </Tooltip>
-          </Link>
 
-          <Link to={"/Profile"}>
-            <Tooltip placement="right" title="Profile">
-              <UserOutlined className="sb-btn" />
-            </Tooltip>
-          </Link>
-          <Link to={"/Home"}>
-            <Tooltip placement="right" title="My Courses">
-              <TeamOutlined className="sb-btn" />
-            </Tooltip>
-          </Link>
-          <Link to={"/"}>
-            <Tooltip placement="right" title="Logout">
-              <LogoutOutlined className="sb-btn" />
-            </Tooltip>
-          </Link>
-        </div>
-      </Sider>
+    return (
+      <div>
+        <Modal
+          title="New Team"
+          onCancel={this.handleCancel}
+          onOk={this.handleCancel}
+          centered
+          visible={this.state.modalVisible}
+          footer={[
+            <Button key="Cancel" onClick={this.handleCancel}>
+              Cancel
+            </Button>,
+            <Button type="primary" onClick={this.handleOk} key="Submit">
+              Submit
+            </Button>,
+          ]}
+        >
+          <CreateTeamForm />
+        </Modal>
+        <Sider
+          className="sideBar theme-background-color"
+          width={120}
+          collapsible={true}
+          collapsedWidth={0}
+        >
+          <div className="sb-profilePictureContainer">
+            <Image src={profilePic} alt="profile picture" />
+          </div>
+          <div className="sideBarButtons">
+            <Link>
+              <Tooltip placement="right" title="New Team">
+                <PlusOutlined className="sb-btn" onClick={this.openModal} />
+              </Tooltip>
+            </Link>
+
+            <Link to={"/Profile"}>
+              <Tooltip placement="right" title="Profile">
+                <UserOutlined className="sb-btn" />
+              </Tooltip>
+            </Link>
+            <Link to={"/Home"}>
+              <Tooltip placement="right" title="My Courses">
+                <TeamOutlined className="sb-btn" />
+              </Tooltip>
+            </Link>
+            <Link to={"/"}>
+              <Tooltip placement="right" title="Logout">
+                <LogoutOutlined className="sb-btn" />
+              </Tooltip>
+            </Link>
+          </div>
+        </Sider>
+      </div>
     );
   }
 }
