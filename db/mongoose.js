a24d196d5577ed44dc2a9f1f6ae7d5d6f2c861d5
Sherman Liu <shermanliu03@gmail.com>
2020-12-04 16:34:33 -0500

    updated MONGOURI in mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 
-const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://admin:admin@cluster0.t3zms.mongodb.net/User?retryWrites=true&w=majority'
+const mongoURI = process.env.MONGODB_URI || "mongodb+srv://shermansjliu:csc309team04@cluster0.f7u4c.mongodb.net/Cluster0?retryWrites=true&w=majority"
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 
